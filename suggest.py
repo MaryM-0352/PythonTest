0edadddfac9e82d54ac1df8b09d92b911c41b81c
MaryM-0352 <113771940+MaryM-0352@users.noreply.github.com>
2023-03-12 17:00:43 +0500

    Update suggest.py

--- suggest.py
+++ suggest.py
@@ -38,32 +38,3 @@ def suggest_handler():
 
 if __name__ == '__main__':
     app.run(debug=DEBUG)
-
-
-import unittest
-import suggest
-
-
-class TestSuggester(unittest.TestCase):
-    def setUp(self):
-        self.my_suggerter = suggest.Suggester(suggest.SUGGEST_FILE)
-
-    def test_maxSize(self):
-        print()
-        self.assertEqual(10, len(self.my_suggerter.get("")))
-
-    def test_empty(self):
-        self.assertListEqual(
-            list(),
-            self.my_suggerter.get(""))
-
-    def test_default(self):
-        self.assertEqual(
-            list(),
-            self.my_suggerter.get(None)
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
-
